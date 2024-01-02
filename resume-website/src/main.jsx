d2e8bc98c8c6a73ee665f84643d8e3a571d4f912
anupam-ahi <anupam.gollapalli@gmail.com>
2024-01-01 21:51:02 -0800

    Created Navbar and added styling to it

--- resume-website/src/main.jsx
+++ resume-website/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <Navbar />
+    <Home />
   </React.StrictMode>,
 )
