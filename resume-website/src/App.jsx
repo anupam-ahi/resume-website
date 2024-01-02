586924506bfe0c4d75ec454610af26e93b4f76fa
anupam-ahi <anupam.gollapalli@gmail.com>
2024-01-02 12:30:35 -0800

    Implemented Routing to the navbar

--- resume-website/src/App.jsx
+++ resume-website/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- resume-website/src/main.jsx
+++ resume-website/src/main.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 import './index.css'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
+import Home from './pages/Home'
+import Education from './pages/Education'
+import Experience from './pages/Experience'
+import Projects from './pages/Projects'
+import ResearchWork from './pages/ResearchWork'
+import Skills from './pages/Skills'
 
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Home />
+  },
+  {
+    path : "education",
+    element : <Education />
+  },
+  {
+    path : "experience",
+    element : <Experience />
+  },
+  {
+    path : "projects",
+    element : <Projects />
+  },
+  {
+    path : "research_work",
+    element : <ResearchWork />
+  },
+  {
+    path : "skills",
+    element : <Skills />
+  },
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router = {router} />
     
-    <Navbar />
-    <Home />
+    
+    
   </React.StrictMode>,
 )
